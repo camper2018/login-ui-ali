7b00840d2dda9f10b1073e1fac3cf4ef745432a2
Tania Ali <taniafali@gmail.com>
2023-09-18 17:32:58 -0700

    Add some UI changes and an image

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
- 
- // code to show alert on form submission
 
+ // code to show alert on form submission
  let submitBtn = document.querySelector('button');
  submitBtn.addEventListener('click', (e) => {
      const form = document.getElementById('login');
      let  username = form.elements['username'].value;
      let password = form.elements['password'].value;
      if (username && password.length >= 8 ) {
-         alert(`Hello President ${username}!\nWelcome to the United States Government Database!`);
+        var body = document.querySelector("body");
+        body.style.margin = "0px";
+        body.innerHTML = `<div class='home'></div>
+                          <div class="home-text">
+                            <h2>Hello President ${username}!</h2>
+                            <h1 style="font-size:50px">Welcome to the United States Government Database!</h1>
+                          </div>`;
+                          
+
+      
      }
      
  });
- 
+
  // code to toggle password visibility
 
  const showPassword = () => {
